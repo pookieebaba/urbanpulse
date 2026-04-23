17a596db75b6318d3489d634f0db8b935ac104ae
pookieebaba <shubham18sood@gmail.com>
2026-04-23 17:05:08 +0530

    Add gunicorn for production

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import os
 from app import create_app, socketio
 
-app = create_app(os.getenv("FLASK_ENV", "development"))
+app = create_app(os.getenv("FLASK_ENV", "production"))
 
 if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
         port=int(os.getenv("PORT", 5000)),
-        debug=app.config["DEBUG"],
-        use_reloader=False,   # disable reloader when using socketio threading
+        debug=False,
+        allow_unsafe_werkzeug=True,
     )
